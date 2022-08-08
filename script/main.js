00756928444cd74cbbad294ab36563fb7304e8b6
alajido <96017433+Alajido@users.noreply.github.com>
2022-08-08 16:25:52 +0100

    first commit

---
+++ script/main.js
@@ -0,0 +1,156 @@
+// on app load, get all task from the localStorage
+window.onload = loadTasks;
+
+// on form Submit, add task
+document.querySelector('form').addEventListener('submit', e =>{
+    // prevent form from submitting the data
+    e.preventDefault();
+    // add tasks to the localStorage, either
+    addTask();
+});
+
+// function to laod tasks on window load
+function loadTasks() {
+    // check if localStorage has any task
+    // if not, then return
+    if (localStorage.getItem('tasks') == null)
+    return;
+
+    // get task from localStorage and convert them to an array
+    let tasks = Array.from(JSON.parse(localStorage.getItem('tasks')));
+
+    // loop through the tasks and add to the list
+    tasks.forEach(task => {
+        // get the ul fro our html document
+        const list = document.querySelector('ul');
+        // creating list item
+        const li = document.createElement('li');
+        // add the li document onto the li element
+        li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+        <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+        <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+
+        //inserting the li into the ul at the first position
+        list.insertBefore(li, list.children[0]);
+    });
+};
+
+// add task function
+function addTask() {
+    // getting the user input
+    const task = document.querySelector('form input');
+    // getting the ul
+    const list = document.querySelector('ul');
+
+    // validating user to make sure something is been entered
+    if (task.value === '' || task.value === " ") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please Add a Task!',
+          })
+        return false;
+    };
+
+    // check if task exist
+    if (document.querySelector(`input[value = "${task.value}"]`)) {
+        Swal.fire({
+            icon: 'info',
+            title: 'Oops...',
+            text: 'Task Already Exist!',
+            // footer: '<a href="">Why do I have this issue?</a>'
+          })
+        return false;
+    };
+
+    // add task to the local storage
+    localStorage.setItem('tasks', JSON.stringify([...JSON.parse(localStorage.getItem('tasks') || '[]'), {task: task.value, completed: false }]));
+    
+    // create li and append to the list
+    const li = document.createElement('li');
+    // add the value to the li element
+    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+    <input type="text" value="${task.value}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+    <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+
+    // insert the li to our list 
+    list.insertBefore(li, list.children[0]);
+    // clear the input field
+    task.value = '';
+};
+
+
+// mark task as completed function
+function taskComplete(e) {
+    let tasks = Array.from(JSON.parse(localStorage.getItem('tasks')));
+
+    tasks.forEach(task => {
+        if (task.task === e.nextElementSibling.value) {
+            task.completed = !task.completed
+        }
+    });
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    e.nextElementSibling.classList.toggle('completed')
+}
+
+// remove task function
+function removeTask(e) {
+    let tasks = Array.from(JSON.parse(localStorage.getItem('tasks')));
+    // loop through each task 
+    tasks.forEach(task => {
+        if (task.task === e.parentNode.children[1].value) {
+            tasks.splice(tasks.indexOf(task), 1);
+        };
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    e.parentElement.remove()
+};
+
+// store current task to track changes
+var currentTask = null;
+
+// get current task
+function getCurrentTask(e) {
+    currentTask = e.value;
+};
+
+// function to edit task and get the current task
+function editTask(e) {
+    let tasks = Array.from(JSON.parse(localStorage.getItem('tasks')));
+
+    // check if user delete the task without typing the updated task
+    if (e.value === '' || e.value === " ") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Tasks is Empty',
+          })
+        e.value === currentTask;
+        return;
+    };
+
+    // task already exist
+    tasks.forEach(task => {
+        if (task.task === e.value) {
+            Swal.fire({
+                icon: 'info',
+                title: 'Oops...',
+                text: 'Task Already Exist!',
+              })
+            e.value === currentTask;
+            return;
+        }
+
+    })
+
+    //update task
+    tasks.forEach(task => {
+        if (task.task === currentTask) {
+            task.task =e.value
+        };
+    });
+
+    // update the local storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
